cd9c05a8c35e1f489a0b904679f2619cafdfff7f
장정안 <wjddks96@naver.com>
2023-08-03 13:37:52 +0900

    feat: searchPage 구현

--- src/Pages/SearchPage/index.js
+++ src/Pages/SearchPage/index.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "../../api/axios";
 
 export default function SearchPage() {
-  return (
-    <div>index</div>
-  )
+    const [searchResults, setSearchResults] = useState([]);
+
+    const useQuery = () => {
+        console.log(useLocation());
+        return new URLSearchParams(useLocation().search);
+      };
+    
+      let query = useQuery();
+      const searchTerm = query.get("q");
+      console.log("searchTerm", searchTerm);
+
+    useEffect(() => {
+        if(searchTerm) {
+            fetchSearchMovie(searchTerm);
+        }
+    },[searchTerm])
+
+    const fetchSearchMovie = async (searchTerm) => {
+        try {
+            const request = await axios.get(
+                `/search/multi?include_adult=false&query=${searchTerm}`
+                )
+                console.log('request', request);
+                setSearchResults(request.data.results);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+  
+  return <div></div>;
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,29 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import "./Nav.css"
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Nav() {
   const [show, handleShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
+    window.addEventListener("scroll", () => {
       window.scrollY > 50 ? handleShow(true) : handleShow(false);
     });
     return () => {
-      window.removeEventListener('scroll', () => {});
-    }
-  },[]);
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`)
+  };
   return (
     <nav className={`nav ${show && "nav__black"}`}>
-      <img 
-      alt='netflix logo'
-      src='//upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png'
-      className='nav__logo'
-      onClick={() => window.location.reload()}
+      <img
+        alt="netflix logo"
+        src="//upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
       />
-      <img 
-        alt='User logged'
-        src='' 
+
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text" 
+        placeholder="영화를 검색해주세요."
       />
+
+      <img alt="User logged" src="" />
     </nav>
-  )
+  );
 }
