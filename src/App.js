8c3b7f99f912a3276d969fe2a8fe10801d27a4a9
장정안 <wjddks96@naver.com>
2023-08-03 13:06:37 +0900

    feat: 페이지 별로 쪼개기

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import React from "react";
 import Nav from "../src/components/Nav";
-import Banner from "./components/Banner";
-import Row from "./components/Row";
-import requests from "./api/requests";
+import { Outlet, Routes, Route } from "react-router-dom";
+import MainPage from "./Pages/MainPage";
+import DetailPage from "./Pages/DetailPage";
+import SearchPage from "./Pages/SearchPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+    </div>
+    //<Footer /> 는 생략
+  );
+};
 
 export default function App() {
   return (
     <div>
-      <Nav /> 
-      <Banner />
-      <Row //FEEDBACK props 관련 강의 들을 필요
-        title="NETFLIX ORIGINALS"
-        id="NO"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row
-        title="Action Movies"
-        id="AM"
-        fetchUrl={requests.fetchActionMovies}
-      />
-      <Row
-        title="Comedy Movies"
-        id="CM"
-        fetchUrl={requests.fetchComedyMovies}
-      />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
